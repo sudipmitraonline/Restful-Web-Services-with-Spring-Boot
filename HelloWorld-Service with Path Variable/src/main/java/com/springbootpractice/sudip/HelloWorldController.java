31984398020327d17424b5f2a4304addc272a410
Sudip Mitra <44464604+sudipmitraonline@users.noreply.github.com>
2022-08-12 22:11:51 +0530

    Hello Service with Path Variable

---
+++ HelloWorld-Service with Path Variable/src/main/java/com/springbootpractice/sudip/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.springbootpractice.sudip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello %s",name));
+	}
+}
