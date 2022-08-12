d23cf2cde8f35fa9273bbdbf466d85cbe93f9ee7
Sudip Mitra <44464604+sudipmitraonline@users.noreply.github.com>
2022-08-12 18:53:07 +0530

    Creating a Hello World Service

---
+++ HelloWorld-Spring-RestfulWebServices/src/main/java/com/springbootpractice/sudip/HelloWorldSpringRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpractice.sudip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringRestfulWebServicesApplication.class, args);
+	}
+
+}
