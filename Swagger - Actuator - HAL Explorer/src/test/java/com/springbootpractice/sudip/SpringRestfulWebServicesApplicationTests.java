b13a25306fd0848f451d02c21ac808fe6c02e570
Sudip Mitra <44464604+sudipmitraonline@users.noreply.github.com>
2022-08-16 17:00:40 +0530

    Swagger, HAL Explorer, Actuator

---
+++ Swagger - Actuator - HAL Explorer/src/test/java/com/springbootpractice/sudip/SpringRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootpractice.sudip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
